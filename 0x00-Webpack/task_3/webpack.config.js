57d7010ef3dc6c481bb9ef35f0e5f2dc13cc29b7
sofy <bedlusofi@gmail.com>
2024-06-22 19:53:30 +0300

    task-3 Dev servers, modules, and tree shaking

---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        header: path.resolve(__dirname, './modules/header/header.js'),
+        body: path.resolve(__dirname, './modules/body/body.js'),
+        footer: path.resolve(__dirname, './modules/footer/footer.js')
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true,
+                            disable: true,
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            chunks: ['header', 'body', 'footer']
+        })
+    ],
+    mode: 'development',
+    performance: {
+        hints: false
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    devServer: {
+        port: 8564,
+        static: path.join(__dirname, 'public'),
+        open: true
+    }
+};

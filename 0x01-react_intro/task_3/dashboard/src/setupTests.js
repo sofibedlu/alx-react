ca5a3bd2b8bad88535f06840b14671ae2f7ad9ee
sofy <bedlusofi@gmail.com>
2024-06-27 16:19:09 +0300

    downgrade react to support enzyme

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,4 +1,4 @@
 import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-18';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({ adapter: new Adapter() });

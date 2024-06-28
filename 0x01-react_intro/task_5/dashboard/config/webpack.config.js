25f4bdc2e233af74cd3da6c6d7ca8e4a4fc88aca
sofy <bedlusofi@gmail.com>
2024-06-28 11:46:42 +0300

    install babel

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -33,7 +33,20 @@ module.exports = {
                 }
               }
         ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
       }
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
 };

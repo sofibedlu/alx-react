b4025271ce328e4bc2baf7e72dff643fd4a4c93a
sofy <bedlusofi@gmail.com>
2024-06-28 11:34:04 +0300

    update-config

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         use: [
+             "file-loader",
              {
                 loader: 'image-webpack-loader',
                 options: {

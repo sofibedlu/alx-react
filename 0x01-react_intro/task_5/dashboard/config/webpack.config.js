7165e156530b0cdd72f3ed215362055842b95a7f
sofy <bedlusofi@gmail.com>
2024-06-28 10:47:33 +0300

    task-8 create a project using webpack

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: '../src/index.js'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: '../dist',
+    hot: true,
+    port: 3000
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+             {
+                loader: 'image-webpack-loader',
+                options: {
+                    bypassOnDebug: true,
+                    disable: true,
+                }
+              }
+        ]
+      }
+    ],
+  },
+};

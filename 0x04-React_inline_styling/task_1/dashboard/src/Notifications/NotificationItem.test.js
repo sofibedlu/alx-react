774cf29121bc6f7bf26f5358b68fb0d58c557c57
sofy <bedlusofi@gmail.com>
2024-09-13 20:52:19 +0300

    testing-cheker for style injection suppresion setup in the test-file

--- 0x04-React_inline_styling/task_1/dashboard/src/Notifications/NotificationItem.test.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Notifications/NotificationItem.test.js
@@ -1,8 +1,17 @@
 import React from "react";
 import NotificationItem from "./NotificationItem";
 import { shallow } from "enzyme";
+import { StyleSheetTestUtils } from 'aphrodite';
 
 describe("NotificationItem", () => {
+    beforeAll(() => {
+        StyleSheetTestUtils.suppressStyleInjection();
+      });
+      
+    afterAll(() => {
+        StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+      });
+      
     it("renders without crashing", () => {
         shallow(<NotificationItem />);
     });

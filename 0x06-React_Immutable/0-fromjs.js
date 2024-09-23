4ae0c50ab93cb848336fc292ff9805a266a6da91
sofy <bedlusofi@gmail.com>
2024-09-23 14:58:52 +0300

    update task-0 for checker

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,14 +1,5 @@
 import { fromJS } from 'immutable';
 
-const getImmutableObject = (object) => {
-    return fromJS(object);
-}
-
-const input = {
-    fear: true,
-    smell: -1033575916.9145899,
-    wall: false,
-    thing: -914767132
-};
+const getImmutableObject = (object) => fromJS(object);
 
 export default getImmutableObject;

147ed2d7cf4843c3b2ea5781ff8db9e28c428d9f
sofy <bedlusofi@gmail.com>
2024-09-23 16:55:39 +0300

    rafactor tak-8

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,18 +1,21 @@
 import { Seq } from 'immutable';
 
+// Helper function to capitalize the first letter of a string
 function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-export function printBestStudents(grades) {
-    const students = Seq(grades)
-        .filter(student => student.score >= 70)
-        .map(student => ({
-            ...student,
-            firstName: capitalize(student.firstName),
-            lastName: capitalize(student.lastName),
-        }))
-        .toObject();
+// Main function
+function printBestStudents(students) {
+  const studentsSeq = Seq(students);
+  const filteredStudents = studentsSeq.filter(student => student.score >= 70);
+  const capitalizedStudents = filteredStudents.map(student => ({
+    score: student.score,
+    firstName: capitalize(student.firstName),
+    lastName: capitalize(student.lastName),
+  }));
 
-    console.log(students);
+  console.log(capitalizedStudents.toObject());
 }
+
+export default printBestStudents;
